41ee6b7087aeefdbddb7467c5d14b64e205a0d3e
caio prezotto <152824103+caioprezottoo@users.noreply.github.com>
2025-06-04 16:50:27 -0300

    TaskManager created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Sidebar } from './components/Sidebar'
+import { Sidebar } from './components/Sidebar.jsx'
+import { TaskManager } from './components/TaskManager.jsx'
+import styles from './App.module.css'
 
 export default function App() {
   return (
-    <div>
+    <div className={styles.appLayout}>
       <Sidebar />
+      <TaskManager />
     </div>
   )
 }
---
+++ src/components/TaskManager.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './TaskManager.module.css'
+
+export const TaskManager = () => {
+  return (
+    <div className={styles.taskManagerBody}>TaskManager</div>
+  )
+}
